3f72809c998aba0e45cfd3b11db8487fb2b4ca90
Huluette <leana.beuvant@gmail.com>
2023-10-12 16:53:06 +0200

    ajout de fichiers et beaucoup d'erreurs

--- src/index.js
+++ src/index.js
@@ -5,14 +5,15 @@ import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './reducers';
 import { getPosts } from './actions/post.action';
-import {getUser} from './actions/user.action';
+import { getUser } from './actions/user.action';
 import { createRoot } from 'react-dom/client';
+import './styles/index.scss';
 
 
 const store = configureStore({
   reducer: rootReducer,
-  devTools: true,
-})
+  devTools: true
+});
 
 store.dispatch(getPosts());
 store.dispatch(getUser());
